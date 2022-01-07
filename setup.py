f7c48f5134ef94b7c02ea39108e22a6513f2b7ce
Christophe Prud'homme <christophe.prudhomme@feelpp.org>
2022-01-07 22:21:54 +0100

    see setup.cfg to see the mtadata

--- setup.py
+++ setup.py
@@ -1,13 +1,3 @@
 from setuptools import setup
 
-setup(
-    name="mo2fmu",
-    version="1.0",
-    py_modules=["mo2fmu"],
-    scripts=['mo2fmu'],
-    include_package_data=True,
-    install_requires=["click","spdlog","xvfbwrapper","pathlib"],
-    entry_points="""
-
-    """,
-)
+setup()
