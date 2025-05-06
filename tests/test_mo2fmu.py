9c19ddf06d37dc1d06015a369c634ca43b7fc034
Philand <philippe.pincon@cemosis.fr>
2025-05-06 11:29:43 +0200

    Update test_mo2fmu.py #7

--- tests/test_mo2fmu.py
+++ tests/test_mo2fmu.py
@@ -1,3 +1,41 @@
 import pytest
-from feelpp.mo2fmu import mo2fmu
+from feelpp.mo2fmu.mo2fmu import mo2fmu
+from pathlib import Path
 
+
+@pytest.mark.parametrize("modelPath, outdirPath", [
+    (Path("src/cases/ode_exp.mo"), Path("src"))
+])
+def test_pathExists(modelPath, outdirPath):
+    assert modelPath.exists()
+    assert outdirPath.exists()
+    print(modelPath)
+
+
+@pytest.mark.parametrize("mo, outdir", [
+    ("src/cases/ode_exp.mo", "src/"),
+])
+def test_basicConversion(mo, outdir):
+    """
+    Test mo2fmu python script using mo file.
+
+    Parameters
+    ----------
+    mo: path
+        path of the modelica file to convert to FMU.
+    outdir: path
+        path of the output file directory.
+    """
+    fmumodelname = None
+    load = None
+    flags = None
+    type = "all"
+    version = "2"
+    dymola = "/opt/dymola-2023-x86_64/"
+    dymolapath = "/usr/local/bin/dymola-2023-x86_64"
+    # dymolaegg = "/data/home/pincon/mo2fmu/dymola_patched.egg"
+    dymolaegg = "Modelica/Library/python_interface/dymola.egg"
+    verbose = True
+    force = True
+
+    mo2fmu(mo, outdir, fmumodelname, load, flags, type, version, dymola, dymolapath, dymolaegg, verbose, force)
