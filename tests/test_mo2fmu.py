448b7fb320d2965fa74f548829ba948d9b92c2e9
Philand <philippe.pincon@cemosis.fr>
2025-06-04 14:36:22 +0200

    Add check of fmu validity in test_mo2fmu.py #7

--- tests/test_mo2fmu.py
+++ tests/test_mo2fmu.py
@@ -1,6 +1,7 @@
 import pytest
 from feelpp.mo2fmu.mo2fmu import mo2fmu
 from pathlib import Path
+from xvfbwrapper import Xvfb
 
 
 @pytest.mark.parametrize("modelPath, outdirPath", [
@@ -38,7 +39,25 @@ def test_basicConversion(mo, outdir):
     force = True
 
     mo2fmu(mo, outdir, fmumodelname, load, flags, type, version, dymola, dymolapath, dymolaegg, verbose, force)
+
     # check if the FMU file is created
     fmu_path = Path(outdir) / "ode_exp.fmu"
     assert fmu_path.exists(), f"FMU file {fmu_path} was not created."
     print(f"FMU file created at: {fmu_path}")
+
+    # launch a display server (needed to launch Dymola)
+    vdisplay = Xvfb()
+    vdisplay.start()
+
+    # check model validity of the fmu
+    from dymola.dymola_interface import DymolaInterface
+    dymApp = DymolaInterface(dymolapath=dymolapath, showwindow=False)
+    importedFMU = dymApp.importFMU(str(fmu_path))
+    print(importedFMU)
+    result = dymApp.checkModel(problem="ode_exp_fmu", simulate=False)
+    if result is False:
+        log = dymApp.getLastErrorLog()
+        print(log)
+    dymApp.close()
+    vdisplay.stop()
+    assert result, f"FMU file {fmu_path} isn't valid, see the log: {log}"
